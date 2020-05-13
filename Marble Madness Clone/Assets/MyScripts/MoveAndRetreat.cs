e443d917c3b9c93693eacc279c0fe4fd2580840a
Alex Silvestri <alexsilvestri.16@gmail.com>
2020-05-13 04:03:34 +0200

    Inserita pushing trap

---
+++ Marble Madness Clone/Assets/MyScripts/MoveAndRetreat.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAndRetreat : MonoBehaviour
+{
+    string state;
+    public float speed1, speed2;
+    public float value1, value2;
+    public float yPosition,xPosition,zPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        value1 = this.transform.localPosition.y + 1;
+        value2 = this.transform.localPosition.y - 1;
+        yPosition = this.transform.position.y;
+        xPosition = this.transform.position.x;
+        zPosition = this.transform.position.z;
+        state = "exit";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Action();
+    }
+
+    void Action()
+    {
+        if (state == "exit")
+        {
+            if (this.transform.localPosition.y‬ < value1)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * speed1);
+            }
+            else
+            {
+                state = "enter";
+            }
+        }
+        else if (state == "enter")
+        {
+            if (this.transform.localPosition.y > value2)
+            {
+                transform.Translate(Vector3.down * Time.deltaTime * speed2);
+            }
+            else
+            {
+                state = "exit";
+                this.transform.position = new Vector3(xPosition,yPosition,zPosition);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Marble Madness Clone/Assets/MyScripts/TrapScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapScript : MonoBehaviour
+{
+    public GameObject[] trapSpikes;
+    public int numberOfSpikes;
+    public int[] randomizedSpikes;
+    public int[] idArray;
+    string state;
+    bool firstTime;
+    float timer;
+    public float maxTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = maxTimer;
+        state = "initialize";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Trap();
+    }
+
+    void Trap()
+    {
+        if (state == "initialize")
+        {
+            firstTime = true;
+            for (int i = 0; i<idArray.Length; i++)
+            {
+                idArray[i] = i;
+                randomizedSpikes[i] = 0;
+            }
+            numberOfSpikes = Random.Range(1, trapSpikes.Length);
+            for (int i = 0; i < numberOfSpikes; i++)
+            {
+                int temp = Random.Range(0, trapSpikes.Length-i);
+                randomizedSpikes[i] = idArray[temp];
+                for (int y = temp; y < trapSpikes.Length-i; y++)
+                {
+                    if (y != trapSpikes.Length - 1 - i)
+                    {
+                        idArray[y] = idArray[y + 1];
+                    }
+                }
+            }
+            state = "move";
+        }
+        else if (state == "move")
+        {
+            if (firstTime == true)
+            {
+                for (int i = 0; i < numberOfSpikes; i++)
+                {
+                    trapSpikes[randomizedSpikes[i]].SetActive(true);
+                }
+                firstTime = false;
+            }
+            if (timer > 0)
+            {
+                timer -= 1 * Time.deltaTime;
+            }
+            if (timer <= 0)
+            {
+                timer = maxTimer;
+                state = "initialize";
+            }
+        }
+    }
+}
